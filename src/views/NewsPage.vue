6e6430d7f4e5dfbb99c7f5956a2472954fdd6ea1
Riswan-Nopiyar <btdbattles777@gmail.com>
2025-01-22 01:27:13 +0800

    style tailwind

--- src/views/NewsPage.vue
+++ src/views/NewsPage.vue
@@ -1,37 +1,39 @@
 <template>
-    <section class="container mx-auto px-4 md:px-20 py-8">
-        <hr class="mb-4 border"/>
-      <div class="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-        <div
-          v-for="(news, index) in newsList"
-          :key="index"
-          class="flex lg:block items-center bg-white shadow-lg rounded-lg overflow-hidden"
-        >
-          <!-- Gambar -->
-          <a :href="news.link" target="_blank" class="block w-32 h-24 lg:h-auto lg:w-full flex-shrink-0">
-            <img
-              :src="news.image"
-              :alt="news.title"
-              class="w-full h-full object-cover border-b border-red-600"
-            />
-          </a>
-  
-          <!-- Konten Berita -->
-          <div class="p-4 flex-1 lg:block">
-            <p class="text-sm text-gray-500 mb-1">{{ news.date }}</p>
-            <a :href="news.link" target="_blank" class="text-sm font-semibold text-gray-800 hover:underline line-clamp-2">
-              {{ news.title }}
-            </a>
-          </div>
+    <NewsSection />
+    <section class="container mx-auto px-4 md:px-20 pb-8">
+        <hr class="mb-4 border" />
+        <div class="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-2 lg:gap-8">
+            <div v-for="(news, index) in newsList" :key="index"
+                class="flex lg:block items-center bg-white rounded-lg overflow-hidden lg:mpl-shadow">
+                <!-- Gambar -->
+                <a :href="news.link" target="_blank" class="block w-32 h-24 lg:h-auto lg:w-full flex-shrink-0">
+                    <img :src="news.image" :alt="news.title"
+                        class="w-full h-full object-cover border-b border-red-600" />
+                </a>
+
+                <!-- Konten Berita -->
+                <div class="p-4 flex-1 lg:block">
+                    <p class="text-sm text-gray-500 mb-1">{{ news.date }}</p>
+                    <a :href="news.link" target="_blank"
+                        class="text-sm font-semibold text-gray-800 hover:underline line-clamp-2">
+                        {{ news.title }}
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
     </section>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent } from "vue";
-  import newsList from "@/assets/json/newsList.json";
-  
+
+  <section class="mt-32 pb-16 ">
+  <SponsorsSection />
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import newsList from "@/assets/json/newsList.json";
+import NewsSection from "@/components/NewsSection.vue";
+import SponsorsSection from "@/components/SponsorsSection.vue";
+
 interface NewsItem {
     date: string;
     title: string;
@@ -39,17 +41,20 @@ interface NewsItem {
     link: string;
 }
 
-  export default defineComponent({
+export default defineComponent({
     name: "NewsPage",
-    data(): { newsList: NewsItem[]} {
-      return {
-        newsList: newsList as NewsItem[],
-      };
+    components: {
+        NewsSection,
+        SponsorsSection
     },
-  });
-  </script>
-  
-  <style scoped>
-  /* Tambahkan styling khusus jika dibutuhkan */
-  </style>
-  
+    data(): { newsList: NewsItem[] } {
+        return {
+            newsList: newsList as NewsItem[],
+        };
+    },
+});
+</script>
+
+<style scoped>
+/* Tambahkan styling khusus jika dibutuhkan */
+</style>
